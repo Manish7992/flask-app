12ca628d2fa578199975d20cc1903cf355d20e17
MANISH KUMAR SINGH <66897947+Manish7992@users.noreply.github.com>
2022-12-05 23:47:41 +0530

    Delete backend directory

--- backend/app/run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-app = Flask(__name__, static_url_path='', static_folder='static')
-CORS(app)
-
-@app.route('/msg/')
-def home():
-   return {'msg': 'This is a message from flask backend.'}
